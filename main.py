da132dbd34e7f4c273f748d74d3d6776da5e3eb8
lightwar <pedrosomluz00@gmail.com>
2026-06-08 19:52:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,33 +4,28 @@ import getpass
 from config import criar_pastas
 from config import organizar_arquivos
 
-#para pegar o nome do usuário do computador e dar as boas vindas!
+# Para pegar o nome do usuário do computador e dar as boas vindas!
 nome_usuario = getpass.getuser()
 print(f"Olá, {nome_usuario}! Seja bem-vindo.")
 
-# O loop 'while True' faz o menu ficar aparecendo repetidamente
+# Caso dê erro ou digite algo alem do solicitado
 while True:
     print("\n=== ASSISTENTE DE ORGANIZAÇÃO ===")
     print("1 - Organizar programas")  
     print("2 - Sair do Programa")
     print("=====================================")
     
-    # Captura a escolha do usuário
     opcao = input("Escolha uma opção (1 ou 2): ")
     
-    # Testando as opções com if/elif/else
     if opcao == "1":
         print("\n[Iniciando organização de Programas...]")
         
-        # 1. Montamos o caminho direto e exclusivo do Downloads: C:\Users\computador\Downloads
         pasta_downloads_exclusiva = os.path.join("C:\\Users", nome_usuario, "Downloads")
         
-        # 2. Passamos ESSE caminho do Downloads para as funções trabalharem apenas nele!
         criar_pastas(pasta_downloads_exclusiva)
         organizar_arquivos(pasta_downloads_exclusiva)        
     elif opcao == "2":
         print(f"\nEncerrando a atividade... Até logo!")
-        break  # O comando 'break' quebra o 'while True' e fecha o programa de vez!
-        
+        break  
     else:
         print(f"\nOpção inválida! Por favor, digite 1 ou 2.")
